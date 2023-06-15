1e97dc8b603dbe33285b9415533c5ee77886efa0
vohizy <hei.vohizy@gmail.com>
2023-06-15 10:57:25 +0300

    chore: delete some import not use

--- src/component/hour.tsx
+++ src/component/hour.tsx
@@ -1,24 +1,8 @@
-import moment from "moment";
-import { GetServerSideProps } from "next";
 /* type de serveur time */
 export type ServerTime = {
   serverTime: string;
 };
 const GetHourServer = ({ serverTime }: ServerTime) => {
   return <>{serverTime}</>;
-};
-
-/* recuper l'heure dans le serveur */
-export const getServerSideProps: GetServerSideProps = async () => {
-  const serverTime = new Date().toISOString();
-
-  console.log("serveur");
-
-  // Retournez les données récupérées en tant que props
-  return {
-    props: {
-      serverTime,
-    },
-  };
 };
 export default GetHourServer;
