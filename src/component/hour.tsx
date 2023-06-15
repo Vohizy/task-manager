64e10afc4ac82f4d1e780d12592aec8bdaefee0d
vohizy <hei.vohizy@gmail.com>
2023-06-15 11:24:39 +0300

    update: update the component

--- src/component/hour.tsx
+++ src/component/hour.tsx
@@ -1,8 +1,12 @@
+import moment from "moment";
+
 /* type de serveur time */
 export type ServerTime = {
   serverTime: string;
 };
 const GetHourServer = ({ serverTime }: ServerTime) => {
+  const date = new Date();
+
   return <>{serverTime}</>;
 };
 export default GetHourServer;
