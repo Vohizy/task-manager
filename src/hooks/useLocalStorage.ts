1dc4e5c16490af685fbf0db9cf8c6193d8014932
vohizy <hei.vohizy@gmail.com>
2023-06-15 11:49:04 +0300

    feat: create type for the useLocalStorage and make the return of getValue and setValue

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,40 @@
-const useLocalStorage = () => {
+type LocalStorageKey = string;
 
-}
+type LocalStorageValue = any;
 
-export {
-  useLocalStorage
-}
+type LocalStorageState = {
+  getValue: (key: LocalStorageKey) => LocalStorageValue | null;
+  setValue: (key: LocalStorageKey, value: LocalStorageValue) => void;
+};
+
+const useLocalStorage = (): LocalStorageState => {
+  /* get the item in the localStorage */
+  const getValue = (key: LocalStorageKey): LocalStorageValue | null => {
+    try {
+      const value = localStorage.getItem(key);
+      return value ? JSON.parse(value) : null;
+    } catch (error) {
+      console.error(
+        `Error getting value from Local Storage for key '${key}':`,
+        error
+      );
+      return null;
+    }
+  };
+  /* set new value in localStorage */
+  const setValue = (key: LocalStorageKey, value: LocalStorageValue): void => {
+    try {
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(key, serializedValue);
+    } catch (error) {
+      console.error(
+        `Error setting value in Local Storage for key '${key}':`,
+        error
+      );
+    }
+  };
+
+  return { getValue, setValue };
+};
+
+export { useLocalStorage };
